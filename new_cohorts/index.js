5d4212502eaf51510b8851801571801dce4ee6dc
Stas Anikin <stas.terrace@gmail.com>
2020-12-31 13:09:51 -0800

    everything is working, need to make it look pretty, get rid of extra code

--- new_cohorts/index.js
+++ new_cohorts/index.js
@@ -25,18 +25,20 @@ const NumberOfTeams = Randomizer.NumberOfTeams;
 const knex = require("./db/client");
 
 //ASSIGNING TEAMS
-app.post('/assign', (request, response, next)=> {
-  // the req.body is the form data received from the submitted form
-  // it's been added to req by the body-parser middleware
+app.get('/assign/:id', (request, response, next)=> {
+  console.log(request.query)
   let error = null;
   let randomizedTeams = [];
-  const {
-      members,
-      number,
-      method
-  } = request.body
-  console.log(request.body);
-  response.cookie('members', request.body.names, {
+  const members = request.query.members;
+  const number = request.query.number;
+  const method= request.query.method;
+  // const {
+  //     members,
+  //     number,
+  //     method
+  // } = request.query
+    console.log(request.query);
+  response.cookie('members', request.query.names, {
       maxAge: 3000000
   });
 
@@ -50,17 +52,23 @@ app.post('/assign', (request, response, next)=> {
           randomizedTeams = NumberOfTeams(members, number)
       }
   }
-  // res.send(req.body);
-  // (NEW!) Shortcut Object creation syntax
-  // { params } creates an object with a property named params and a value equal
-  // to params (i.e. { params: params })
-  console.log(randomizedTeams)
 
-  response.redirect(`view/${cohort.id}`, {
+  console.log(randomizedTeams)
+  knex("cohorts")
+  .where("id", request.params.id)
+  .first()
+  .then((cohort) => {
+    if (!cohort) {
+      response.send("No such cohort");
+    } else {
+        response.render(`assign/${cohort.id}`, {
       members: randomizedTeams,
       method: request.body.method,
       number: request.body.number,
       error: error
+    })
+  };
+
   });
   // passing an object as a second argument to render will make all properties
   // of that object available as variables inside the template
--- new_cohorts/routes/cohorts.js
+++ new_cohorts/routes/cohorts.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const knex = require('../db/client');
 const router = express.Router();
-
+const Randomizer = require('../randomizer.js');
+const randomizeTeams = Randomizer.RandomizeTeams;
+const NumberOfTeams = Randomizer.NumberOfTeams;
 // Index
+router.get('/', (request, response)=>{
+  response.redirect('/cohorts')
+})
 router.get("/cohorts", (request, response) => {
   knex("cohorts")
     .orderBy("id", "desc")
@@ -10,8 +15,6 @@ router.get("/cohorts", (request, response) => {
       response.render("cohorts", { cohorts: cohorts });
     });
 });
-
-
 
 // GET REQUESTS
 router.get("/home", (request, response) => {
@@ -21,7 +24,7 @@ router.get("/new_cohort", (request, response) => {
   response.render("new_cohort", { cohort: false });
 });
 
-// CREATE - this code works, above is experimental
+// CREATE
 
 router.post("/create", (request, response) => {
 
@@ -48,21 +51,172 @@ router.post("/create", (request, response) => {
   });
   
   });
+
+
+
+  // this code works, but does not give the correct result
+  router.get("/view/:id", (request, response) => {
+    const cohortId=request.params.id;
+    const method=request.query.method;
+    const quantity=parseInt(request.query.quantity)
+    let error = null;
+    let randomizedTeams = [];
+    const number=quantity;
+
+
+    console.log(method, quantity, cohortId);
+    if (!method){
+      {
+          knex("cohorts")
+            .where("id", request.params.id)
+            .first()
+            .then((cohort) => {
+              if (!cohort) {
+                response.send("No such cohort");
+              } else {
+                response.render("view", { cohort: cohort,
+                  new_team: [],
+                  quantity:'',
+                  method:'' });
+              }
+            });
+        };
+
+    } else 
+    
+
+    
+    {
+      knex
+      .select("*")
+      .from("cohorts")
+      .where({ id: cohortId })
+      .then(results => {
+        console.log(results);
+        const [cohort]=results;
+        let members=cohort.members;
+        let members_arr=(cohort.members).split(',').map(member=>member.trim());
+
+        console.log(members_arr.length)
+        if (quantity>members_arr.length){
+          response.render('home')
+          error = 'number of teams must be between 1 and number of members'
+        } else {
+        function shuffleArray(members_arr){
+          for (let i=members_arr.length-1; i>0; i--){
+            let j=Math.floor(Math.random()*(i+1));
+            let temp=members_arr[i];
+            members_arr[i]=members_arr[j];
+            members_arr[j]=temp;
+          }
+          return members_arr;
+        }
+        members_arr=shuffleArray(members_arr);
+        let new_team =[];
+        if (method==='per_team'){
+          randomizedTeams = randomizeTeams(members, number)
+
+          while(members_arr.length){new_team.push(members_arr.splice(0, quantity))}
+        } else {          randomizedTeams = NumberOfTeams(members, number)
+
+          let new_length=(members_arr.length/quantity)
+        while(members_arr.length>0&&new_length<=members_arr.length){
+          // console.log('new length', new_length);
+          // console.log('array length', members_arr.length);
+          // console.log(new_length<=members_arr.length);
+          new_team.push(members_arr.splice(0, new_length));
+          new_length=Math.ceil((members_arr.length/quantity))
+        }
+        }
+// console.log(new_team);
+response.render('view', {
+  cohort:cohort||{},
+  new_team: new_team||[],
+  quantity:quantity,
+  method:method,
+  members: randomizedTeams,
+  number:number
+})
+    }})}
+  })  
+
+  //   // this code works, but does not give the correct result
+  //   router.get("/view/:id", (request, response) => {
+  //     const cohortId=request.params.id;
+  //     const method=request.query.method;
+  //     const quantity=parseInt(request.query.quantity)
+  //     console.log(method, quantity, cohortId);
+  //     if (!method){
+  //       {
+  //           knex("cohorts")
+  //             .where("id", request.params.id)
+  //             .first()
+  //             .then((cohort) => {
+  //               if (!cohort) {
+  //                 response.send("No such cohort");
+  //               } else {
+  //                 response.render("view", { cohort: cohort,
+  //                   new_team: [],
+  //                   quantity:'',
+  //                   method:'' });
+  //               }
+  //             });
+  //         };
   
-  
-  
-router.get("/view/:id", (request, response) => {
-  knex("cohorts")
-    .where("id", request.params.id)
-    .first()
-    .then((cohort) => {
-      if (!cohort) {
-        response.send("No such cohort");
-      } else {
-        response.render("view", { cohort: cohort });
-      }
-    });
-});
+  //     } else {
+  //       knex
+  //       .select("*")
+  //       .from("cohorts")
+  //       .where({ id: cohortId })
+  //       .then(results => {
+  //         console.log(results);
+  //         const [cohort]=results;
+  //         let members_arr=(cohort.members).split(',').map(member=>member.trim());
+  //         function shuffleArray(members_arr){
+  //           for (let i=members_arr.length-1; i>0; i--){
+  //             let j=Math.floor(Math.random()*(i+1));
+  //             let temp=members_arr[i];
+  //             members_arr[i]=members_arr[j];
+  //             members_arr[j]=temp;
+  //           }
+  //           return members_arr;
+  //         }
+  //         members_arr=shuffleArray(members_arr);
+  //         let new_team =[];
+  //         if (method==='per_team'){
+  //           while(members_arr.length){new_team.push(members_arr.splice(0, quantity))}
+  //         } else {
+  //           let new_length=(members_arr.length/quantity)
+  //         while(members_arr.length>0&&new_length<=members_arr.length){
+  //           // console.log('new length', new_length);
+  //           // console.log('array length', members_arr.length);
+  //           // console.log(new_length<=members_arr.length);
+  //           new_team.push(members_arr.splice(0, new_length));
+  //           new_length=Math.ceil((members_arr.length/quantity))
+  //         }
+  //         }
+  // // console.log(new_team);
+  // response.render('view', {
+  //   cohort:cohort||{},
+  //   new_team: new_team||[],
+  //   quantity:quantity,
+  //   method:method
+  // })
+  // })}
+  //   })  
+  // the code below works, above is experimental
+// router.get("/view/:id", (request, response) => {
+//   knex("cohorts")
+//     .where("id", request.params.id)
+//     .first()
+//     .then((cohort) => {
+//       if (!cohort) {
+//         response.send("No such cohort");
+//       } else {
+//         response.render("view", { cohort: cohort });
+//       }
+//     });
+// });
 
 // EDIT
 router.get("/edit/:id", (request, response) => {
