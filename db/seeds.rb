370d73ec2de98d301510765db85d8a2497f795b6
Stas Anikin <stas.terrace@gmail.com>
2021-01-27 19:10:30 -0800

    posts crud done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all()
+
+50.times do
+  title = Faker::Hipster.sentence
+  body = Faker::Hipster.paragraph_by_chars(characters: 256, supplemental: false)
+  Post.create(
+    title: title,
+    body: body,
+  )
+end
+posts = Post.all
+puts "created #{posts.count} posts"
