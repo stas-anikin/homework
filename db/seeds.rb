cfe92f520c973d8bf89444e493c35156cb8d8698
Stas Anikin <stas.terrace@gmail.com>
2021-01-29 09:35:06 -0800

    week 5 homework done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Post.destroy_all()
+Comment.destroy_all()
 
 50.times do
   title = Faker::Hipster.sentence
   body = Faker::Hipster.paragraph_by_chars(characters: 256, supplemental: false)
-  Post.create(
+  p = Post.create(
     title: title,
     body: body,
   )
+  if p.valid?
+    p.comments = rand(1..10).times.map do
+      Comment.new(
+        body: Faker::Hipster.paragraph,
+      )
+    end
+  end
 end
 posts = Post.all
-puts "created #{posts.count} posts"
+comments = Comment.all
+puts "created #{posts.count} posts, #{comments.count} comments"
